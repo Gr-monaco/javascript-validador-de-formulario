2accfb2f7f83bfa3b26a9809cf66fb636054fa1c
Gabriel Monaco <grmonaco@stefanini.com>
2022-02-02 20:00:24 -0300

    Adiciona constantes de forms Adicionado constantes userName, userSurname, userCpf e userUser para facilitar uso futuro dos elementos

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const userForm = document.forms["form"];
+const userName = userForm["fname"];
+const userSurname = userForm["fsurname"];
+const userCpf = userForm["fcpf"];
+const userUser = userForm["fuser"];
