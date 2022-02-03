82fee7583425282ddde89239f2d7798734fcecc8
Gabriel Monaco <grmonaco@stefanini.com>
2022-02-02 22:29:00 -0300

    Adiciona função de validação - Adicionada função validadeName() na qual verifica se o form de nome recebeu um nome valido, ou seja, o input não está vazio - Adicionada função addErrorText(), na qual recebe o objeto, que no caso deve ser um form e adiciona uma mensagem de texto dentro de uma unordered list

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,28 @@ const userName = userForm["fname"];
 const userSurname = userForm["fsurname"];
 const userCpf = userForm["fcpf"];
 const userUser = userForm["fuser"];
+
+let userNameValid = false;
+
+function validadeName(){
+    userName.value = userName.value.trim(); // Retira excesso de espaço
+    if (userName.value === ""){
+        if (!(userName.parentElement.getElementsByClassName("error-warming").length)){ //verifica se não existe erros dentro do objeto
+            addErrorText(userName.parentElement, "Nome deve ser preenchido");
+        }
+    }else{
+        if(userName.parentElement.getElementsByClassName("error-warming").length){ //Pode não remover todos os erros se por acaso for adicionado outro erro no mesmo div
+            userName.parentElement.removeChild(userName.parentElement.getElementsByClassName("error-warming")[0]); //Pega o parente e remove o elemento filho dentro da lista
+        }
+    }
+}
+
+function addErrorText(obj, text){
+    let ul = document.createElement("ul");
+    ul.classList.add("error-warming")
+    let p = document.createElement("p");
+
+    p.innerText = text;
+    ul.appendChild(p);
+    obj.appendChild(ul);
+}
