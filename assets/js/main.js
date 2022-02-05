e4e15f96edd4e429a8280254a147367e5136dfac
Gabriel Monaco <gr-monaco@hotmail.com>
2022-02-04 22:23:18 -0300

    Adiciona validação de cpf

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,10 @@ const valid = {
 }
 
 function start(){
-    userName.addEventListener("blur", function() {validadeName(userNameValid ,userName, "Nome deve estar preenchido.", 'userName')});
-    userSurname.addEventListener("blur", function() {validadeName(userSurnameValid ,userSurname, "Sobrenome deve estar preenchido.", 'userSurname')});
+    userName.addEventListener("blur", function() {validadeName(userNameValid, userName, "Nome deve estar preenchido.", 'userName')});
+    userSurname.addEventListener("blur", function() {validadeName(userSurnameValid, userSurname, "Sobrenome deve estar preenchido.", 'userSurname')});
     userUser.addEventListener("blur", function () {validadeName(usernameLenght, userUser, "Usuário deve ter entre 3 a 12 caractéres.", "userUser")});
+    userCpf.addEventListener("blur", function () {validadeName(validateCPF, userCpf, "Insira um CPF válido.", "userCpf")});
 }
 
 function usernameLenght(){
@@ -35,6 +36,38 @@ function userSurnameValid(){
     return userSurname.value !== "" ? true : false;
 }
 
+function validateCPF() {
+    let [pv1, pv2] = userCpf.value.split('-');
+    pv1 = pv1.replace('.','').replace('.','');
+    let array_pv1 = pv1.split('');
+    let array_pv2 = pv2.split('');
+    let array_rep = array_pv1.concat(array_pv2);
+    if(array_rep.every(f => f === array_rep[0])) return false;
+    
+
+    let p1 = userCpf.value.split('-')[0].replace('.', '').replace('.', '');
+    let array_p1 = p1.split('');
+    array_p1 = array_p1.reverse()
+    let soma = array_p1.map(f => parseInt(f)).reduce(f = (prev, current, index, array) => {
+        //console.log(array[index], index+2, prev, current*(index+2));
+        return prev += current * (index + 2);
+    },0);
+
+    let middle = (soma % 11 > 9) ? 0 : soma;
+
+    if (!(11 - middle%11 === parseInt(userCpf.value.split('-')[1][0]))) return false;
+
+    array_p1.unshift(userCpf.value.split('-')[1][0])
+    soma = array_p1.map(f => parseInt(f)).reduce(f = (prev, current, index, array) => {
+        return prev += current * (index + 2);
+    },0);
+
+    middle = (soma % 11 === 0) ? 0 : soma;
+    if (!(11 - middle%11 === parseInt(userCpf.value.split('-')[1][1]))) return false;
+
+    return true;
+}
+
 function validadeName(validationFunction ,obj, errorText, validationBoolKey){  // divisão de função não deixaria mais simples? Complexidade de código fica reduzida, talvez vale repetição
     if (!validationFunction()){
         if (!(obj.parentElement.getElementsByClassName("error-warming").length)){ //verifica se não existe erros dentro do objeto
@@ -51,6 +84,7 @@ function validadeName(validationFunction ,obj, errorText, validationBoolKey){  /
     }
     enableButton();
 }
+
 
 function removeError(obj){
     obj.parentElement.removeChild(obj.parentElement.getElementsByClassName("error-warming")[0]);
